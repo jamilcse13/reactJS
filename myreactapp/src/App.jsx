ac81dc62e693ad233c1e43ee8ed7700f88db8dad
jamil <jamil.bd0561@gmail.com>
2023-05-02 15:56:22 +0600

    cleaning unnecessary codes

--- myreactapp/src/App.jsx
+++ myreactapp/src/App.jsx
@@ -1,9 +1,4 @@
 import "./App.css";
-import Calculator from "./components/LiftingState/Calculator";
-// import Text from "./components/inheritence/Text";
-import Emoji from "./components/composition/Emoji";
-import Bracket from "./components/composition/Bracket";
-import Text from "./components/composition/Text";
 
 // without hoc
 // import ClickCounter from "./components/hoc/norrmal-process/ClickCounter";
@@ -14,22 +9,8 @@ import HoverCounter from "./components/hoc/hoc-process/HoverCounter";
 
 
 function App() {
-  // const quantities = [1,2,3];
-  // return <ClockList quantities={quantities} locale="bn-BD" >Bangladesh</ClockList>;
   return (
     <div>
-      {/* <Form/> */}
-      {/* <Calculator /> */}
-      {/* inheritance */}
-      {/* <Text/> */}
-
-      {/* <Emoji>
-        {({addEmoji}) => (
-            <Bracket>
-              {({addBracket}) => <Text addEmoji={addEmoji} addBracket={addBracket}/>}
-            </Bracket>)}
-      </Emoji>       */}
-
       <ClickCounter/>
       <HoverCounter/>
     </div>
--- myreactapp/src/components/LiftingState/BoilingVerdict.jsx
+++
@@ -1,6 +0,0 @@
-export default function BoilingVerdict({celcius = 0}) {
-    if (celcius >= 100) {
-        return <p>The water would boil.</p>
-    }
-    return <p>The water would not boil.</p>
-}
