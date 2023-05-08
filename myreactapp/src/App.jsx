77d8a6abdebf38423d7a7ad387cdedea40dbcdac
jamil <jamil.bd0561@gmail.com>
2023-05-08 16:21:02 +0600

    useEffect hook

--- myreactapp/src/App.jsx
+++ myreactapp/src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+// import MyComponent from "./components/MyComponentClass";
+import MyComponent from "./components/MyComponent";
 
 function App() {
-  return <div>Hello World</div>;
+  const [show, setShow] = useState(true);
+
+  return (
+    <div className="app">
+      <div>{show && <MyComponent/>}</div>
+      <p>
+        <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+          {show ? 'Hide Post' : 'Show Post'}
+        </button>
+      </p>
+    </div>
+  );
 }
 
 export default App;
