84bcfe8a79ffae7d4db69d30db99382f1fb345a2
jamil <jamil.bd0561@gmail.com>
2023-05-09 19:16:44 +0600

    useRef hook

--- myreactapp/src/App.jsx
+++ myreactapp/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+// import Form from "./components/Form";
+import Time from "./components/Time";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="app">
+      <div>
+        <Time/>
+      </div>
+    </div>
+  );
 }
 
 export default App;
